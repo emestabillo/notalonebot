982feb4dfd6ac99fe5612f3b95bc026b10aee7dd
emestabillo <emsestabillo@gmail.com>
2018-06-08 19:41:04 -0400

    Setup twitterbot

---
+++ twitterbot.js
@@ -0,0 +1,41 @@
+const twit = require('twit');
+
+const config = {
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret
+}
+
+const Twitter = new twit(config);
+
+let retweet = function() {
+  let params = {
+    q: 'katespade, anthonybourdain',
+    result_type: 'popular',
+    lang: 'en'
+  }
+
+  Twitter.get('search/tweets', params, function(err,data) {
+    if (!err) {
+      for (let i = 0; i < 3; i++) {
+        let rtId = data.statuses[i].id_str;
+        Twitter.post('statuses/retweet/:id', {
+          id: rtId
+          }, function(err,response) {
+          if (response) {
+            console.log('Successfully retweeted');
+          }
+          if (err) {
+            console.log(err);
+          }
+        });
+        }
+      }
+      else {
+      console.log('Could not search tweets');
+    }
+  });
+}
+retweet();
+setInterval(retweet, 600000);
